98ee2e1e8c29579a4dd52835104d9fe6eb3e0f5a
Joshua Dibble <jdibby@gmail.com>
2024-10-09 04:32:45 -0600

    Added new tutorial

---
+++ if-list.py
@@ -0,0 +1,21 @@
+"""
+pizza = ["cheese", "pepperoni", "sausage", "banana"]
+
+pizza_answer = input("What is your favorite pizza topping?  ").lower()
+
+for toppings in pizza:
+    if answer == toppings:
+        print("We have that!")
+    if answer != toppings:
+        print("We do NOT have that!")
+        break
+"""
+
+feelings = ["happy", "sad", "angry", "excited", "anxious", "content", "frustrated", "hopeful", "lonely", "curious", "grateful", "bored", "overwhelmed", "inspired", "confused", "relaxed", "nervous", "joyful", "disappointed", "fearful"]
+
+feeling_answer = input("How are you feeling?  ").lower()
+
+if feeling_answer in feelings:
+    print("I understand that feeling! =)")
+else:
+    print("I don't understand that feeling! =(")
