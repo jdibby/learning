3b548f8fbebc17a7263949c76c9a8e93d0b6de6f
Joshua Dibble <jdibby@gmail.com>
2024-10-08 19:24:22 -0600

    print test

--- hello.py
+++ hello.py
@@ -1,1 +1,21 @@
-print("Hello, World!")
+import time
+import os
+"""
+print("Hello world")
+Hi = "Hello world!"
+
+print(Hi)
+"""
+Name = input("What is your name?   ")
+
+Feeling = input(f"how are you {Name}?   ")
+#answer to why needs comment on direct awnser
+
+if Feeling == "Good":
+    print(f"Thats good to hear {Name}")
+    
+    why = input(f"So why are you {Feeling} {Name}?")
+    time.sleep(5)
+    quit
+
+os.system('clear')
