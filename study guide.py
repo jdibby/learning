8f68f95758e91a50ef9f23cdb62b90d1bcae0a66
Joshua Dibble <jdibby@gmail.com>
2024-10-09 21:05:33 -0600

    test

--- study guide.py
+++ study guide.py
@@ -1,8 +1,44 @@
 print(">>>>>>>>")
 print("5a quiz")
 print(">>>>>>>>")
-anwser = input("what is grandparents in spanish     ")
-if anwser == "los abuelos":
+answer = input("what is grandparents in spanish     ").lower()
+if answer == "los abuelos":
+    print(f"CORRECT")
+else:
+    print(f"INCORRECT")
+
+answer1 = input("what is grandfather in spanish     ").lower()
+if answer1 == "el abuelo":
+    print(f"CORRECT")
+else:
+    print(f"INCORRECT")
+
+answer2 = input("what is grandmother in spanish     ").lower()
+if answer2 == "la abuela":
+    print(f"CORRECT")
+else:
+    print(f"INCORRECT")
+
+answer3 = input("what is husband in spanish     ").lower()
+if answer3 == "el esposo":
+    print(f"CORRECT")
+else:
+    print(f"INCORRECT")
+
+answer4 = input("what is wife in spanish     ").lower()
+if answer4 == "la esposa":
+    print(f"CORRECT")
+else:
+    print(f"INCORRECT")
+
+answer5 = input("what is brothers and sisters in spanish     ").lower()
+if answer5 == "los hermanos":
+    print(f"CORRECT")
+else:
+    print(f"INCORRECT")
+
+answer6 = input("what is brother in spanish     ").lower()
+if answer6 == "el hermano":
     print(f"CORRECT")
 else:
     print(f"INCORRECT")
