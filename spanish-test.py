8490ebb2a81f5fa56e80f7163f5b8686e5286ab6
jdibby <48450541+jdibby@users.noreply.github.com>
2024-10-10 07:59:23 -0600

    counting

--- spanish-test.py
+++ spanish-test.py
@@ -7,17 +7,17 @@ translations = {
     }
 
 def spanish_test():
-    score = 0
+    right = 0
+    wrong = 0
 
     for english_word, spanish_word in translations.items():
         answer = input(f"What is '{english_word}' in Spanish?  ").lower()
         if answer == spanish_word:
             print("Woooohoooo!")
-            score += 1
-            print(score)
+            right += 1
         else:
             print(f"You suck! The answer is '{spanish_word}'.")
-            print(score)
+            wrong += 1
 
     print("Your total score:", score)
 
