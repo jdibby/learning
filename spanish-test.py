3799deb5907d058b395d447bd8e9a63272ee3a1d
Joshua Dibble <jdibby@gmail.com>
2024-10-10 08:27:22 -0600

    added percentages

--- spanish-test.py
+++ spanish-test.py
@@ -14,12 +14,21 @@ def spanish_test():
         answer = input(f"What is '{english_word}' in Spanish?  ").lower()
         if answer == spanish_word:
             print("Woooohoooo!")
+            print("")
             right += 1
         else:
             print(f"You suck! The answer is '{spanish_word}'.")
+            print("")
             wrong += 1
 
-    print("Your total score:", right)
+    total_questions = right + wrong
+    if total_questions > 0:
+        score_percentage = (right / total_questions) * 100
+    else:
+        score_percentage = 0
 
+    print(f"Your total score: {right} correct, {wrong} incorrect.")
+    print(f"Your percentage score: {score_percentage:.2f}%")
+    print("")
 if __name__ == "__main__":
     spanish_test()
