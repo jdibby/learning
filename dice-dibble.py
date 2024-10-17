b4c50b6a040fb9c62b33775821a43599fc70e5ec
jdibby <48450541+jdibby@users.noreply.github.com>
2024-10-17 16:18:55 -0600

    new dice game updates

--- dice-dibble.py
+++ dice-dibble.py
@@ -1,45 +1,53 @@
 import random
 
+start_playing = input("Do you want to play a dice game? (y/n").lower()
+
+count = 0
+
+if start_playing == "y" and count < 10000:
+    number = random.randint(1,6)
+    print("test")
+
 keep_playing = "y"
 
 while keep_playing == "y":
     number = random.randint(1,6)
     if number == 1:
-        print("[-------]")
-        print("[       ]")
-        print("[   0   ]")
-        print("[       ]")
-        print("[-------]")
+        print("/-------\\")
+        print("|       |")
+        print("|   0   |")
+        print("|       |")
+        print("\-------/")
     elif number == 2:
-        print("[-------]")
-        print("[ 0     ]")
-        print("[       ]")
-        print("[     0 ]")
-        print("[-------]")
+        print("/-------\\")
+        print("| 0     |")
+        print("|       |")
+        print("|     0 |")
+        print("\-------/")
     elif number == 3:
-        print("[-------]")
-        print("[       ]")
-        print("[ 0 0 0 ]")
-        print("[       ]")
-        print("[-------]")
+        print("/-------\\")
+        print("| 0     |")
+        print("|   0   |")
+        print("|     0 |")
+        print("\-------/")
     elif number == 4:
-        print("[-------]")
-        print("[ 0   0 ]")
-        print("[       ]")
-        print("[ 0   0 ]")
-        print("[-------]")
+        print("/-------\\")
+        print("| 0   0 |")
+        print("|       |")
+        print("| 0   0 |")
+        print("\-------/")
     elif number == 5:
-        print("[-------]")
-        print("[ 0   0 ]")
-        print("[   0   ]")
-        print("[ 0   0 ]")
-        print("[-------]")
+        print("/-------\\")
+        print("| 0   0 |")
+        print("|   0   |")
+        print("| 0   0 |")
+        print("\-------/")
     elif number == 6:
-        print("[-------]")
-        print("[ 0 0 0 ]")
-        print("[       ]")
-        print("[ 0 0 0 ]")
-        print("[-------]")
+        print("/-------\\")
+        print("| 0 0 0 |")
+        print("|       |")
+        print("| 0 0 0 |")
+        print("\-------/")
     else:
         break 
     keep_playing = input("press y to roll again and n to exit:")
