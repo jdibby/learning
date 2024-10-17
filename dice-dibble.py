e805fc162ed61c031e47c3ddfd1809522a875b54
jdibby <48450541+jdibby@users.noreply.github.com>
2024-10-17 16:23:15 -0600

    update rolling

--- dice-dibble.py
+++ dice-dibble.py
@@ -10,47 +10,47 @@ if start_playing == "y":
         number = random.randint(1,6)
         count += 1
 
-keep_playing = "y"
-
-while keep_playing == "y":
-    number = random.randint(1,6)
-    if number == 1:
-        print("/-------\\")
-        print("|       |")
-        print("|   0   |")
-        print("|       |")
-        print("\-------/")
-    elif number == 2:
-        print("/-------\\")
-        print("| 0     |")
-        print("|       |")
-        print("|     0 |")
-        print("\-------/")
-    elif number == 3:
-        print("/-------\\")
-        print("| 0     |")
-        print("|   0   |")
-        print("|     0 |")
-        print("\-------/")
-    elif number == 4:
-        print("/-------\\")
-        print("| 0   0 |")
-        print("|       |")
-        print("| 0   0 |")
-        print("\-------/")
-    elif number == 5:
-        print("/-------\\")
-        print("| 0   0 |")
-        print("|   0   |")
-        print("| 0   0 |")
-        print("\-------/")
-    elif number == 6:
-        print("/-------\\")
-        print("| 0 0 0 |")
-        print("|       |")
-        print("| 0 0 0 |")
-        print("\-------/")
-    else:
-        break 
-    keep_playing = input("press y to roll again and n to exit:")
-    print("\n")
+def roll_dice():
+    keep_playing = "y"
+    while keep_playing == "y":
+        number = random.randint(1,6)
+        if number == 1:
+            print("/-------\\")
+            print("|       |")
+            print("|   0   |")
+            print("|       |")
+            print("\-------/")
+        elif number == 2:
+            print("/-------\\")
+            print("| 0     |")
+            print("|       |")
+            print("|     0 |")
+            print("\-------/")
+        elif number == 3:
+            print("/-------\\")
+            print("| 0     |")
+            print("|   0   |")
+            print("|     0 |")
+            print("\-------/")
+        elif number == 4:
+            print("/-------\\")
+            print("| 0   0 |")
+            print("|       |")
+            print("| 0   0 |")
+            print("\-------/")
+        elif number == 5:
+            print("/-------\\")
+            print("| 0   0 |")
+            print("|   0   |")
+            print("| 0   0 |")
+            print("\-------/")
+        elif number == 6:
+            print("/-------\\")
+            print("| 0 0 0 |")
+            print("|       |")
+            print("| 0 0 0 |")
+            print("\-------/")
+        else:
+            break 
+        keep_playing = input("press y to roll again and n to exit:")
+        print("\n")
