8d690da96943fa160d4aaebb822788c58021bcca
jdibby <48450541+jdibby@users.noreply.github.com>
2024-10-17 16:24:14 -0600

    roll dice updates

--- dice-dibble.py
+++ dice-dibble.py
@@ -1,6 +1,6 @@
 import random
 
-start_playing = input("Do you want to play a dice game? (y/n").lower()
+start_playing = input("Do you want to play a dice game? (y/n)".lower()
 
 count = 0
 
@@ -9,6 +9,7 @@ if start_playing == "y":
     while count < 100000:
         number = random.randint(1,6)
         count += 1
+        roll_dice()
 
 def roll_dice():
     keep_playing = "y"
