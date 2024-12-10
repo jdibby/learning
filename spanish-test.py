0296e48db7f80394876d37df5d1598bec90875cf
mdibby <mdibby11@gmail.com>
2024-12-09 20:03:18 -0700

    new spanish words

--- spanish-test.py
+++ spanish-test.py
@@ -61,7 +61,7 @@ def mason_spanish_test():
         "market": "el mercado",
         "exit": "la salida",
         "light": "claro",
-        "solid-colored": "de solo un color",
+        "solid-colored": "de solo un colors",
         "dark": "oscuro",
         "pastel": "pastel",          
         "bright": "vivo",
