7e4200ae95311e0be389fec000e3efdc5dc12464
cdibby <cdibby12@gmail.com>
2025-01-29 20:31:39 -0700

    Adding ability to track what was removed

--- spanish-test.py
+++ spanish-test.py
@@ -112,6 +112,8 @@ def mason_spanish_test():
                 print("CORRECT")
                 right += 1
                 correct_count[english_word] += 1  # Increment correct count for this word
+                if correct_count[english_word] >= 2
+                    print("THIS WORD HAS BEEN REMOVED FROM YOUR LIST")
             else:
                 print(f"INCORRECT '{spanish_word}'.")
                 wrong += 1
